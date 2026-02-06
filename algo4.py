10b81e7c182df73c7347157c59942ff53000e947
sala 105 <sala105@poliacademico.edu.loc>
2026-02-05 19:18:43 -0500

    Primer comentario del repositorio

---
+++ algo4.py
@@ -0,0 +1,16 @@
+print("hola mundo")
+a= 1
+if a==1:
+    print("la variable es igual a 1")
+
+print("aqui termina el condicional")
+while a<10:
+    print("sigo en ciclo")
+    a=a+1
+print("aqui termina el condicional")
+for i in range (0,10):
+    print("sigo en ciclo")
+    print(i)
+print("aqui termina el condicional")  
+  
+            
---
+++ clase.py
@@ -0,0 +1,8 @@
+class Estudiante:
+    def __init__(self, nombre , documento):
+        self.nombre=nombre
+        self.documento=documento
+estudiante1=Estudiante("Juan",123)
+print(estudiante1.nombre)        
+
+
